6bd731f00e161d6ecbb9d1347b675785065b4869
juandozuna <juandanielozuna2@gmail.com>
2024-04-04 13:08:00 -0400

    Added the rest of the crud methods
    Signed-off-by: juandozuna <juandanielozuna2@gmail.com>

--- Camco/Camco.Services.CouponAPI/Controllers/CouponController.cs
+++ Camco/Camco.Services.CouponAPI/Controllers/CouponController.cs
@@ -40,5 +40,73 @@ namespace Camco.Services.CouponAPI.Controllers
                 return new ResponseDto<CouponDto?> { IsSuccess = false, Message = e.Message };
             }
         }
+        
+        [HttpGet]
+        [Route("get-by-code/{code}")]
+        public ResponseDto<CouponDto?> Get(string code)
+        {
+            try
+            {
+                Coupon? obj = db.Coupons.FirstOrDefault(c => c.CouponCode == code);
+                var mapped = mapper.Map<CouponDto?>(obj);
+                return new ResponseDto<CouponDto?> { Data = mapped };
+            }
+            catch (Exception e)
+            {
+                return new ResponseDto<CouponDto?> { IsSuccess = false, Message = e.Message };
+            }
+        }
+        
+        [HttpPost]
+        public ResponseDto<CouponDto> Post(CouponDto dto)
+        {
+            try
+            {
+                Coupon obj = mapper.Map<Coupon>(dto);
+                db.Coupons.Add(obj);
+                db.SaveChanges();
+                var mapped = mapper.Map<CouponDto>(obj);
+                return new ResponseDto<CouponDto> { Data = mapped };
+            }
+            catch (Exception e)
+            {
+                return new ResponseDto<CouponDto> { IsSuccess = false, Message = e.Message };
+            }
+        }
+        
+        [HttpPut]
+        public ResponseDto<CouponDto> Put(CouponDto dto)
+        {
+            try
+            {
+                Coupon obj = mapper.Map<Coupon>(dto);
+                db.Coupons.Update(obj);
+                db.SaveChanges();
+                var mapped = mapper.Map<CouponDto>(obj);
+                return new ResponseDto<CouponDto> { Data = mapped };
+            }
+            catch (Exception e)
+            {
+                return new ResponseDto<CouponDto> { IsSuccess = false, Message = e.Message };
+            }
+        }
+        
+        [HttpDelete]
+        [Route("{id:int}")]
+        public ResponseDto<CouponDto?> Delete(int id)
+        {
+            try
+            {
+                Coupon? obj = db.Coupons.Find(id);
+                db.Coupons.Remove(obj);
+                db.SaveChanges();
+                var mapped = mapper.Map<CouponDto?>(obj);
+                return new ResponseDto<CouponDto?> { Data = mapped };
+            }
+            catch (Exception e)
+            {
+                return new ResponseDto<CouponDto?> { IsSuccess = false, Message = e.Message };
+            }
+        }
     }
 }
